be7b40c917c13eb4e178c7338a9e80fadab0deca
isabel <isabel@MintEntornos>
2018-03-03 15:34:41 +0100

    Editado.
    Añadidas las funciones solicitadas de potencia y ToString al objeto
    Calculadora.
    
    En Programa se han añadido las funciones para que las imprima todas por
    pantalla.
    
    En Calculadora también hemos modificado las variables de
    entorno/privacidad para que pudieran ser usadas en Programa.

--- PracticaGitHUBJavadoc/src/Calculadora/Programa.java
+++ PracticaGitHUBJavadoc/src/Calculadora/Programa.java
@@ -1,11 +1,21 @@
 package Calculadora;
 
 public class Programa {
+	public static void main(String[] args) {
+		
+	Calculadora calculo= new Calculadora(6,8);
+	System.out.println(calculo.suma());
+	System.out.println(calculo.resta());
+	System.out.println(calculo.division());
+	System.out.println(calculo.potencia());
+	System.out.println(calculo.toString());
+	
 	
 	//TODO: Crear un programa principal que 
 	//utilice todas las funciones de la calculadora
 	//Incluida las operaciones toString y la potencia
-	//que habréis añadido.
+	//que habrï¿½is aï¿½adido.
 	//Ejecutadlo.
 
+	}
 }
